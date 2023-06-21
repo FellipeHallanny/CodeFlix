5737baeda9f181d76ef9c440830527d28579205e
Fellipe Hallanny Silva Batista <fellipehallanny@gmail.com>
2023-06-21 20:44:55 -0300

    Erro ao não encontrar categoria

--- tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/GetCategoryById/GetCategoryApiTest.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/GetCategoryById/GetCategoryApiTest.cs
@@ -3,6 +3,7 @@ using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
 using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace FC.Codeflix.Catalog.EndToEndTests.Api.Category.GetCategoryById;
@@ -35,5 +36,24 @@ public class GetCategoryApiTest
         output.Description.Should().Be(exampleCategory.Description);
         output.IsActive.Should().Be(exampleCategory.IsActive);
         output.CreatedAt.Should().BeSameDateAs(exampleCategory.CreatedAt);
+    }
+    
+    [Fact(DisplayName = nameof(ThrowWhenNotFound))]
+    [Trait("EndToEnd/API ", "Category/Get - Endpoints")]
+    public async Task ThrowWhenNotFound()
+    {
+        var exampleCategoryList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoryList);
+        var randomGuid = Guid.NewGuid();
+        
+        var (response, output) = await _fixture.ApiClient.Get<ProblemDetails>($"/Categories/{randomGuid}");
+        
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status404NotFound);
+        output.Should().NotBeNull();
+        output!.Status.Should().Be(StatusCodes.Status404NotFound);
+        output.Title.Should().Be("Not Found.");
+        output.Detail.Should().Be($"Category '{randomGuid}' not found.");
+        output.Type.Should().Be("NotFound");
     }
 }
